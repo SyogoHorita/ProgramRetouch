12fd7c92f188957383c440115f39a2e714c20baa
SyogoHorita <akifumi232@gmail.com>
2018-07-24 18:49:46 +0900

    課題2

--- EC/src/ec/UserData.java
+++ EC/src/ec/UserData.java
@@ -1,6 +1,7 @@
 package ec;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import beans.BuyDataBeans;
+import beans.ItemDataBeans;
 import beans.UserDataBeans;
+import dao.BuyDAO;
+import dao.BuyDetailDAO;
 import dao.UserDAO;
 
 /**
@@ -32,6 +37,13 @@ public class UserData extends HttpServlet {
 			// 更新確認画面から戻ってきた場合Sessionから取得。それ以外はuserIdでユーザーを取得
 			UserDataBeans udb = session.getAttribute("returnUDB") == null ? UserDAO.getUserDataBeansByUserId(userId) : (UserDataBeans) EcHelper.cutSessionAttribute(session, "returnUDB");
 
+
+			BuyDataBeans bdb = (BuyDataBeans) EcHelper.cutSessionAttribute(session, "bdb");
+			int buyId = BuyDAO.insertBuy(bdb);
+			BuyDataBeans resultBDB = BuyDAO.getBuyDataBeansByBuyId(buyId);
+			request.setAttribute("resultBDB", resultBDB);
+			ArrayList<ItemDataBeans> buyIDBList = BuyDetailDAO.getItemDataBeansListByBuyId(buyId);
+			request.setAttribute("buyIDBList", buyIDBList);
 
 
 			// 入力された内容に誤りがあったとき等に表示するエラーメッセージを格納する
